30a190e542a095c404cbc17e231efa274f8cbf32
Joachim <joachim.frances@hotmail.be>
2021-12-06 15:30:30 +0100

    3 petits points

---
+++ src/decouverte/java/troisPetitsPoints/TestTroisPP.java
@@ -0,0 +1,44 @@
+package decouverte.java.troisPetitsPoints;
+
+public class TestTroisPP
+{
+    public static void main(String[] args)
+    {
+        //ancienne technique
+        String[] tab = new String[]{"st1", "st2", "st3"};
+        String result = concatAnc(tab);
+
+        //nouvelle technique
+        String res2 = concatNew("st1", "st2", "st3");
+
+        System.out.println(result);
+        System.out.println(res2);
+    }
+
+    //ancienne methode
+    private static String concatAnc(String[] tab)
+    {
+        String result = "";
+
+        for(String s : tab)
+        {
+            result += s;
+        }
+
+        return result;
+    }
+
+    //nouvelle methode
+    private static String concatNew(String... tab)
+    {
+        String result = "";
+
+        for(String s : tab)
+        {
+            result += s;
+        }
+
+        return result;
+    }
+
+}
